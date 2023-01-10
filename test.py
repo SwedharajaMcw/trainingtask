f239293af09922cf69ff9b58f3584c6b97ac11bd
SwedharajaMcw <122254925+SwedharajaMcw@users.noreply.github.com>
2023-01-10 15:26:44 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+# C:\Users\ELCOT\Downloads\input\images-optional
+
+import cv2
+i = cv2.imread("C:\\Users\\ELCOT\\Downloads\\input\\images-optional\\2007_000032.jpg")
+cv2.imshow('image',i)
+cv2.waitKey(0)
